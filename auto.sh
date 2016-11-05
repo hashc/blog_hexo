e1c3594a217500efd03afcd9e2ee1694220382f3
hashc <hash.choi@gmail.com>
2016-11-05 23:32:10 +0800

    some new

--- auto.sh
+++ auto.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "some new"
-git subtree push --prefix=themes/next next master
+git subtree push --prefix=themes/next next master --squash
 git push origin master
 hexo generate
 hexo deploy
